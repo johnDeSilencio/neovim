11f1b426bfe657fbceb6f8f41edc358072030c32
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-11-05 22:44:14 -0800

    Tweak neotree settings to use the most up-to-date settings

--- config/plugins/neotree.nix
+++ config/plugins/neotree.nix
@@ -1,23 +1,30 @@
 {
   plugins.neo-tree = {
     enable = true;
-    enableDiagnostics = true;
-    enableGitStatus = true;
-    enableModifiedMarkers = true;
-    enableRefreshOnWrite = true;
-    closeIfLastWindow = true;
-    # Options: "NC", "double", "none", "rounded", "shadow", "single", or "solid"
-    popupBorderStyle = "rounded";
-    buffers = {
-      bindToCwd = false;
-      followCurrentFile = {
-        enabled = true;
+
+    settings = {
+      enable_diagnostics = true;
+      enable_git_status = true;
+      enable_modified_markers = true;
+      enable_refresh_on_write = true;
+      close_if_last_window = true;
+
+      buffers = {
+        followCurrentFile = {
+          enabled = true;
+        };
+
+        bindToCwd = false;
       };
-    };
-    window = {
-      width = 40;
-      height = 15;
-      autoExpandWidth = false;
+
+      window = {
+        width = 40;
+        height = 15;
+        autoExpandWidth = false;
+      };
+
+      # Options: "NC", "double", "none", "rounded", "shadow", "single", or "solid"
+      popup_border_style = "rounded";
     };
   };
 }
