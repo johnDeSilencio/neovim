189926521815ca5ed2c9af0abeaa4f00fe8a43cc
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-12-04 08:23:15 -0800

    Add key mappings to cmp plugin

--- config/plugins/cmp/default.nix
+++ config/plugins/cmp/default.nix
@@ -3,12 +3,32 @@
     cmp = {
       enable = true;
 
-      settings.sources = [
-        { name = "buffer"; }
-        { name = "calc"; }
-        { name = "nvim_lsp"; }
-        { name = "path"; }
-      ];
+      settings = {
+        mapping = {
+          # Select the [n]ext item
+          "<C-n>" = "cmp.mapping.select_next_item()";
+          # Select the [p]revious item
+          "<C-p>" = "cmp.mapping.select_prev_item()";
+
+          # Scroll the documentation [u]p or [d]own
+          "<C-u>" = "cmp.mapping.scroll_docs(4)";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+
+          # Exit completion
+          "<C-e>" = "cmp.mapping.abort()";
+
+          # Accept completion
+          "<CR>" = "cmp.mapping.confirm { select = true }";
+          "<C-Space>" = "cmp.mapping.confirm {}";
+        };
+
+        sources = [
+          { name = "buffer"; }
+          { name = "calc"; }
+          { name = "nvim_lsp"; }
+          { name = "path"; }
+        ];
+      };
     };
   };
 
