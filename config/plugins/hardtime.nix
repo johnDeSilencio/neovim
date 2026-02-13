2b02725067b733cf1d099bfc267ec469bbad7b5b
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2026-02-12 20:35:41 -0800

    Disable arrow keys in normal and insert mode

--- config/plugins/hardtime.nix
+++ config/plugins/hardtime.nix
@@ -2,6 +2,30 @@
   plugins = {
     hardtime = {
       enable = true;
+
+      settings = {
+        disabled_keys = {
+          "<Up>" = [
+            "n"
+            "i"
+          ];
+
+          "<Down>" = [
+            "n"
+            "i"
+          ];
+
+          "<Left>" = [
+            "n"
+            "i"
+          ];
+
+          "<Right>" = [
+            "n"
+            "i"
+          ];
+        };
+      };
     };
   };
 }
