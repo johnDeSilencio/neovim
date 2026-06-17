541ee2a30da831f2f2a43717ae1715e096a22f6f
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2026-06-17 07:34:38 -0700

    Comment out transparent-nvim for now until the most recent commit is in nixpkgs-unstable

--- config/plugins/transparent.nix
+++ config/plugins/transparent.nix
@@ -1,53 +1,54 @@
 {
-  plugins = {
-    transparent = {
-      enable = true;
-
-      settings = {
-        groups = [
-          "Normal"
-          "NormalNC"
-          "Comment"
-          "Constant"
-          "Special"
-          "Identifier"
-          "Statement"
-          "PreProc"
-          "Type"
-          "Underlined"
-          "Todo"
-          "String"
-          "Function"
-          "Conditional"
-          "Repeat"
-          "Operator"
-          "Structure"
-          "LineNr"
-          "NonText"
-          "SignColumn"
-          "CursorLineNr"
-          "StatusLine"
-          "StatusLineNC"
-          "EndOfBuffer"
-        ];
-        exclude_groups = [
-          "CursorLine"
-        ];
-        extra_groups = [
-          "BufferLineTabClose"
-          "BufferLineBufferSelected"
-          "BufferLineFill"
-          "BufferLineBackground"
-          "BufferLineSeparator"
-          "BufferLineIndicatorSelected"
-        ];
-      };
-
-      luaConfig = {
-        post = ''
-          require("transparent").clear_prefix("BufferLineDevIcon")
-        '';
-      };
-    };
-  };
+  # TODO: Uncomment when newest transparent-nvim plugin is in nixpkgs
+  #   plugins = {
+  #     transparent = {
+  #       enable = true;
+  #
+  #       settings = {
+  #         groups = [
+  #           "Normal"
+  #           "NormalNC"
+  #           "Comment"
+  #           "Constant"
+  #           "Special"
+  #           "Identifier"
+  #           "Statement"
+  #           "PreProc"
+  #           "Type"
+  #           "Underlined"
+  #           "Todo"
+  #           "String"
+  #           "Function"
+  #           "Conditional"
+  #           "Repeat"
+  #           "Operator"
+  #           "Structure"
+  #           "LineNr"
+  #           "NonText"
+  #           "SignColumn"
+  #           "CursorLineNr"
+  #           "StatusLine"
+  #           "StatusLineNC"
+  #           "EndOfBuffer"
+  #         ];
+  #         exclude_groups = [
+  #           "CursorLine"
+  #         ];
+  #         extra_groups = [
+  #           "BufferLineTabClose"
+  #           "BufferLineBufferSelected"
+  #           "BufferLineFill"
+  #           "BufferLineBackground"
+  #           "BufferLineSeparator"
+  #           "BufferLineIndicatorSelected"
+  #         ];
+  #       };
+  #
+  #       luaConfig = {
+  #         post = ''
+  #           require("transparent").clear_prefix("BufferLineDevIcon")
+  #         '';
+  #       };
+  #     };
+  #   };
 }
